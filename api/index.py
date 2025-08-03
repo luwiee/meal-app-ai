81b0c3efffbda177e31640e10e4608ee652ed9cf
LUWIE <lawrencemiguel17@gmail.com>
2025-08-03 13:49:48 +0800

    vercel

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app as app
-
-# Vercel will use the `app` variable as the WSGI entry point.
